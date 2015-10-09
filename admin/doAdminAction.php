773fbea5f2477b35bf65f2e23c39f2734ed8d5b8
陈勇 <wo.imyong@gmail.com>
2015-10-09 17:31:02 +0800

    添加管理员

---
+++ admin/doAdminAction.php
@@ -0,0 +1,52 @@
+<?php 
+require_once '../include.php';
+checkLogined();
+$act=$_REQUEST['act'];
+$id=$_REQUEST['id'];
+if($act=="logout"){
+	logout();
+}elseif($act=="addAdmin"){
+	$mes=addAdmin();
+}elseif($act=="editAdmin"){
+	$mes=editAdmin($id);
+}elseif($act=="delAdmin"){
+	$mes=delAdmin($id);
+}elseif($act=="addCate"){
+	$mes=addCate();
+}elseif($act=="editCate"){
+	$where="id={$id}";
+	$mes=editCate($where);
+}elseif($act=="delCate"){
+	$mes=delCate($id);
+}elseif($act=="addPro"){
+	$mes=addPro();
+}elseif($act=="editPro"){
+	$mes=editPro($id);
+}elseif($act=="delPro"){
+	$mes=delPro($id);
+}elseif($act=="addUser"){
+	$mes=addUser();
+}elseif($act=="delUser"){
+		$mes=delUser($id);
+}elseif($act=="editUser"){
+	$mes=editUser($id);	
+}elseif($act=="waterText"){
+	$mes=doWaterText($id);
+}elseif($act=="waterPic"){
+	$mes=doWaterPic($id);
+}
+?>
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<?php 
+	if($mes){
+		echo $mes;
+	}
+?>
+</body>
+</html>
